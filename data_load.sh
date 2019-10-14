29cbbc82210d1738a8501981d2aa16fbca1cb647
EC2 Default User <ec2-user@ip-172-31-28-189.us-east-2.compute.internal>
2019-10-14 04:18:37 +0000

    pass

--- data_load.sh
+++ data_load.sh
@@ -1,3 +1,3 @@
 #! /bin/bash
-export PGPASSWORD=<your_password>;
-psql -h redshift_endpoint -U awsuser -d redshiftcourse -p 5439 -f data_load.sql
+export PGPASSWORD=Redshift123;
+psql -h redshift-course.cqrzxhtnttfd.us-east-2.redshift.amazonaws.com -U redshiftcourse -d redshiftcourse -p 5439 -f data_load.sql
